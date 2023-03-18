276ade9cde3677ff687ce2dd94da6aa426cd6aff
Esraamohamed0 <esraaelsyed320@gmail.com>
2023-03-18 18:54:29 +0200

    lab1

---
+++ resources/views/post/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('title') Show @endsection
+
+@section('content')
+<form action="{{route('posts.index')}}">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Titel</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" >
+  </div>
+  <br>
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Describtion</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+  <br>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Post creator</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" >
+  </div>
+  <br>
+  <button type="submit" class="btn btn-success"></a>create</button>
+</form>
+@endsection
